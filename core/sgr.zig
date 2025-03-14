eaaa2c718e200475b0adef38e3c668482b7d33f4
Atlas Yu <pseudoc@163.com>
2025-03-14 17:49:12 +0800

    refactor(core): sgr: shorten everything

--- core/sgr.zig
+++ core/sgr.zig
@@ -3,17 +3,27 @@ const CSI = @import("control.zig").CSI;
 
 const SGR = @This();
 
-bold: ?bool = null,
-faint: ?bool = null,
-italic: ?bool = null,
-underline: ?bool = null,
-blink: ?bool = null,
-inverse: ?bool = null,
-conceal: ?bool = null,
-strike: ?bool = null,
+/// Bold (1) Unbold (22)
+bd: ?bool = null,
+/// Faint (2) Unfaint (22)
+ft: ?bool = null,
+/// Italic (3) Unitalic (23)
+it: ?bool = null,
+/// Underline (4) Ununderline (24)
+ul: ?bool = null,
+/// Blink (5) Unblink (25)
+bl: ?bool = null,
+/// Inverse (7) Uninverse (27)
+rv: ?bool = null,
+/// Conceal (8) Reveal (28)
+hd: ?bool = null,
+/// Strike (9) Unstrike (29)
+st: ?bool = null,
 
-foreground: ?Color = null,
-background: ?Color = null,
+/// Foreground color
+fg: ?Color = null,
+/// Background color
+bg: ?Color = null,
 
 fn Buffer(comptime N: u8) type {
     return struct {
@@ -24,38 +34,16 @@ fn Buffer(comptime N: u8) type {
 
         fn init(sgr: SGR) Self {
             var buffer = Self{};
-            if (sgr.bold) |enable| {
-                buffer.append(if (enable) 1 else 22);
-            }
-            if (sgr.faint) |enable| {
-                buffer.append(if (enable) 2 else 22);
-            }
-            if (sgr.italic) |enable| {
-                buffer.append(if (enable) 3 else 23);
-            }
-            if (sgr.underline) |enable| {
-                buffer.append(if (enable) 4 else 24);
-            }
-            if (sgr.blink) |enable| {
-                buffer.append(if (enable) 5 else 25);
-            }
-            if (sgr.inverse) |enable| {
-                buffer.append(if (enable) 7 else 27);
-            }
-            if (sgr.conceal) |enable| {
-                buffer.append(if (enable) 8 else 28);
-            }
-            if (sgr.strike) |enable| {
-                buffer.append(if (enable) 9 else 29);
-            }
-
-            if (sgr.foreground) |color| {
-                buffer.appendColor(30, color);
-            }
-            if (sgr.background) |color| {
-                buffer.appendColor(40, color);
-            }
-
+            if (sgr.bd) |enable| buffer.append(if (enable) 1 else 22);
+            if (sgr.ft) |enable| buffer.append(if (enable) 2 else 22);
+            if (sgr.it) |enable| buffer.append(if (enable) 3 else 23);
+            if (sgr.ul) |enable| buffer.append(if (enable) 4 else 24);
+            if (sgr.bl) |enable| buffer.append(if (enable) 5 else 25);
+            if (sgr.rv) |enable| buffer.append(if (enable) 7 else 27);
+            if (sgr.hd) |enable| buffer.append(if (enable) 8 else 28);
+            if (sgr.st) |enable| buffer.append(if (enable) 9 else 29);
+            if (sgr.fg) |color| buffer.appendColor(30, color);
+            if (sgr.bg) |color| buffer.appendColor(40, color);
             return buffer;
         }
 
@@ -67,9 +55,24 @@ fn Buffer(comptime N: u8) type {
 
         fn appendColor(self: *Self, offset: u16, color: Color) void {
             switch (color) {
-                .c16 => |case| {
-                    self.append(offset + @intFromEnum(case));
-                },
+                .black => self.append(offset + 0),
+                .red => self.append(offset + 1),
+                .green => self.append(offset + 2),
+                .yellow => self.append(offset + 3),
+                .blue => self.append(offset + 4),
+                .magenta => self.append(offset + 5),
+                .cyan => self.append(offset + 6),
+                .white => self.append(offset + 7),
+
+                .b_black => self.append(offset + 60),
+                .b_red => self.append(offset + 61),
+                .b_green => self.append(offset + 62),
+                .b_yellow => self.append(offset + 63),
+                .b_blue => self.append(offset + 64),
+                .b_magenta => self.append(offset + 65),
+                .b_cyan => self.append(offset + 66),
+                .b_white => self.append(offset + 67),
+
                 .c256 => |case| {
                     const params = self.data[self.i..];
                     std.debug.assert(params.len >= 3);
@@ -78,6 +81,7 @@ fn Buffer(comptime N: u8) type {
                     params[2] = case;
                     self.i += 3;
                 },
+
                 .rgb => |case| {
                     const params = self.data[self.i..];
                     std.debug.assert(params.len >= 5);
@@ -88,9 +92,8 @@ fn Buffer(comptime N: u8) type {
                     params[4] = case[2];
                     self.i += 5;
                 },
-                .reset => {
-                    self.append(offset + 9);
-                },
+
+                .reset => self.append(offset + 9),
             }
         }
 
@@ -114,27 +117,24 @@ pub fn format(
 }
 
 pub const Color = union(enum) {
-    const Color16 = enum(u8) {
-        black = 0,
-        red = 1,
-        green = 2,
-        yellow = 3,
-        blue = 4,
-        magenta = 5,
-        cyan = 6,
-        white = 7,
+    black,
+    red,
+    green,
+    yellow,
+    blue,
+    magenta,
+    cyan,
+    white,
 
-        bright_black = 60,
-        bright_red = 61,
-        bright_green = 62,
-        bright_yellow = 63,
-        bright_blue = 64,
-        bright_magenta = 65,
-        bright_cyan = 66,
-        bright_white = 67,
-    };
+    b_black,
+    b_red,
+    b_green,
+    b_yellow,
+    b_blue,
+    b_magenta,
+    b_cyan,
+    b_white,
 
-    c16: Color16,
     c256: u8,
     rgb: [3]u8,
     reset,
@@ -146,26 +146,23 @@ test SGR {
     const E = @import("control.zig").Expect(SGR);
     const cases = .{
         .{ "\x1b[m", SGR{} },
-        .{ "\x1b[1m", SGR{ .bold = true } },
-        .{
-            "\x1b[31m",
-            SGR{ .foreground = .{ .c16 = .red } },
-        },
+        .{ "\x1b[1m", SGR{ .bd = true } },
+        .{ "\x1b[31m", SGR{ .fg = .red } },
         .{
             "\x1b[38;5;1m",
-            SGR{ .foreground = .{ .c256 = 1 } },
+            SGR{ .fg = .{ .c256 = 1 } },
         },
         .{
-            "\x1b[38;2;255;0;0m",
-            SGR{ .foreground = .{ .rgb = .{ 255, 0, 0 } } },
+            "\x1b[38;2;11;23;58m",
+            SGR{ .fg = .{ .rgb = .{ 11, 23, 58 } } },
         },
         .{
             "\x1b[39m",
-            SGR{ .foreground = .reset },
+            SGR{ .fg = .reset },
         },
         .{
             "\x1b[1;31m",
-            SGR{ .bold = true, .foreground = .{ .c16 = .red } },
+            SGR{ .bd = true, .fg = .red },
         },
     };
 
